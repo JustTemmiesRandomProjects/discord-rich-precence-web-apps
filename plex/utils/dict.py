72dac6e2b5a94d4bcec22e51e1b722da6fe89a8c
Temmie <47639983+JustTemmie@users.noreply.github.com>
2023-09-14 09:45:58 +0200

    Delete plex directory

--- plex/utils/dict.py
+++
@@ -1,8 +0,0 @@
-from typing import Any
-
-def merge(source: Any, target: Any) -> None:
-	for key, value in source.items():
-		if isinstance(value, dict):
-			merge(value, target.setdefault(key, {}))
-		else:
-			target[key] = source[key]
